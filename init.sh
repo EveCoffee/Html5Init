34ac9d6129db64dd18aff8c3bd376afef3a5883a
coolcoffee <b5SaggaWpie8>
2016-06-30 16:20:42 +0800

    增加常用的scss文件

--- init.sh
+++ init.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
 
+# 初始化基本文件夹
 mkdir scss css images js es2015
+
+# 下载需要的各种依赖
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/.babelrc
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/config.rb
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/package.json
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/gulpfile.babel.js
 
+# 下载常用的scss的文件
+cd scss
+wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/scss/_style.scss
+wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/scss/_mixins.scss
+wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/scss/_function.scss
+cd ..
+
 npm install
 npm install gulp-sass
+
+wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/init.sh
