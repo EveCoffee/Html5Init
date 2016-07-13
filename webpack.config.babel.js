2604b93d320aca6f166c2ae17238bee67e245313
coolcoffee <b5SaggaWpie8>
2016-07-13 14:46:03 +0800

    集成了TypeScript以及Webpack对scss的支持。

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,16 +1,26 @@
 import path from "path";
+import fs from "fs";
 
 export default {
-    entry: {
-        'index': './es2015/index.js',
-        "easel": "./es2015/easel.js"
-    },
+    entry: (function () {
+        var entry = {};
+
+        fs.readdirSync("es2015/")
+            .filter(_path  => {
+                return fs.statSync(`es2015/${_path}`).isFile();
+            })
+            .map(_path =>{
+                entry[path.parse(_path).name] = `./es2015/${_path}`;
+                return `./es2015/${_path}`;
+            });
+        return entry;
+    })(),
     output: {
         path: path.join(__dirname, '/js'),
         filename: '[name].js'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx', '.ts', '.tsx']
     },
     module: {
         loaders: [
@@ -26,6 +36,14 @@ export default {
                 // could be useful elsewhere in our app, so I usually don't.
                 //include: path.resolve(__dirname, 'node_modules/pixi.js'),
                 loader: 'json'
+            },
+            {
+                test: /\.ts|tsx?$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
             }
         ]
     },
