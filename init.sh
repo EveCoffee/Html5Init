8a7a0dcfbbaa841257710bba756d2e8adecdbd48
王超毅 <wangchaoyi.tk@gmail.com>
2016-12-12 06:18:16 +0000

    升级typescript到2.1.4, 支持直接将await编译出es5代码

--- init.sh
+++ init.sh
@@ -10,10 +10,12 @@ wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/tsconfig.json
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/package.json
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/gulpfile.babel.js
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/webpack.config.babel.js
+
+cd js
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/js/common.js
 
 # 下载常用的scss的文件
-cd scss
+cd ../scss
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/scss/_style.scss
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/scss/_mixins.scss
 wget https://raw.githubusercontent.com/EveCoffee/Html5Init/master/scss/_function.scss
